4845acd994867e73550c2d80986b2f7d0a9ab435
Michael Lei <michaelleie0ca@gmail.com>
2023-11-12 18:05:29 -0800

    Add client-side rendering and active link highlighting to NavLinks component.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,15 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { ForwardRefExoticComponent, RefAttributes, SVGProps } from "react";
+import { UrlObject } from "url";
+import clsx from "clsx";
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { ForwardRefExoticComponent, RefAttributes, SVGProps } from "react";
-import { UrlObject } from "url";
 
 type NavLink = {
   name: string;
@@ -31,6 +35,7 @@ const links: NavLink[] = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -39,7 +44,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
