e879fe89975729781441a8e904e924ecf83a93bc
Michael Lei <michaelleie0ca@gmail.com>
2023-11-15 23:26:16 -0800

    Add metadata to pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,12 @@ import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Acme Dashboard",
-  description: "The official Next.js Course Dashboard, built with App Router.",
-  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://nextjs-dashboard-michaelleii.vercel.app/"),
 };
 
 export default function RootLayout({
