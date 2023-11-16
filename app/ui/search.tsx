99a1106c0cfd08ed8dfae0acd435f990395df5a9
Michael Lei <michaelleie0ca@gmail.com>
2023-11-15 20:50:11 -0800

    Add search query parameter to URL

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
 
     term ? params.set("query", term) : params.delete("query");
+
+    const searchUrl = `${pathname}?${params.toString()}`;
+
+    replace(searchUrl as __next_route_internal_types__.RouteImpl<string>);
   }
 
   return (
@@ -23,6 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
